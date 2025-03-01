19d99cab9b69233dad2d4813c5c0a44019d79ce6
N3wee <ox24oy1i@students.codeinstitute.net>
2025-03-01 15:09:50 +0000

    Update finance_tracker/urls.py to include register route

--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from transactions.views import RegisterView
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),  # Add this line for authentication URLs
